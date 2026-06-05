4afbfd8ecfe9dfd3064167759f9cb5eea7b850c9
mohamed amine elabed <mohamedamineelabed2015@gmail.com>
2026-06-05 11:58:03 +0100

    wouuuuuuh

--- frontend/src/util/constants.js
+++ frontend/src/util/constants.js
@@ -1,6 +1,7 @@
 export const Appconstants = {
    //BACKEND_URL: 'http://localhost:8080/api/v1.0', // Exemple d'URL de backend
     //BACKEND_URL: "https://fragrant-collapse-cuddly.ngrok-free.dev/api/v1.0", // Exemple avec ngrok
-    BACKEND_URL: "https://backend-pfe-production-55e7.up.railway.app/api/v1.0", // Use env variable or fallback to localhost
+    //BACKEND_URL: "https://backend-pfe-production-55e7.up.railway.app/api/v1.0", // Use env variable or fallback to localhost
+    BACKEND_URL: "https://projet-pfe-production-81ca.up.railway.app/api/v1.0", // Use env variable or fallback to localhost
 
 };
